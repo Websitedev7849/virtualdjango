74d5a8094dc5752a1be94cec24b745f50276d2a5
Siddharth Pawar <websitedev7849@gmail.com>
2021-12-31 21:04:11 +0530

    set initial time on about page

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ww*_*w@^82%0(0v*qsmf^lwu0ncqh38ba4!4obpw4_$#anp71h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['websitedev7849.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- project/project/views.py
+++ project/project/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse, QueryDict
 import threading
+from datetime import datetime
+
 
 class HourCounter:
     def __init__(self):
+        self.initial_date = datetime.today()
+        self.initial_time = datetime.now()
+        print(self.initial_time)
         self.hour_count = 0
 
     def incrementCount(self):
@@ -29,5 +34,10 @@ def home(request):
     return HttpResponse("Home page")
 
 def getHourCount(request):
-    return HttpResponse(f"hour_count : {hour_counter.hour_count}")
+    return HttpResponse(f"initial_time: {hour_counter.initial_time}; initial_date: {hour_counter.initial_date}; hour_count : {hour_counter.hour_count}")
 
+"""
+/home/Websitedev7849/virtualdjango/project
+/home/Websitedev7849/virtualdjango/project/project
+mysite-virtualenv
+"""
